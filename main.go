d17fc19324e904c2411a1c4da0654d69124826ab
Douglas Grogg <dgrogg@Douglass-Air.lan>
2017-01-21 21:08:27 -0500

    chat client now works with websockets and some barebones js for the client

--- main.go
+++ main.go
@@ -2,22 +2,37 @@ package main
 
 import "net/http"
 import "log"
+import "sync"
+import "text/template"
+import "path/filepath"
+import "flag"
+
+//templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+//ServeHTTp handles the HTTP Request
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-            <html>
-                <head>
-                    <title>Chat</title>
-                </head>
-                <body>
-                    Lets chat!
-                </body>
-            </html>
-        `))
-	})
+	var addr = flag.String("addr", ":8080", "The addr of the application")
+	flag.Parse()
+	r := newRoom()
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	//get the room going
+	go r.run()
 	//start the server
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Starting web server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
